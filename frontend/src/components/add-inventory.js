50af1f78c5c66c6305d625dd288a45081002c4cf
Anthony <78623358+anthonyrepo@users.noreply.github.com>
2022-01-23 15:20:12 -0500

    Add initial code to components file

---
+++ frontend/src/components/add-inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddInventory() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default AddInventory;
---
+++ frontend/src/components/inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Inventory() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default Inventory;
