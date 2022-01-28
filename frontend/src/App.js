b0582affd923adc1ed9736598920ece83e7a1591
Anthony <78623358+anthonyrepo@users.noreply.github.com>
2022-01-27 23:06:48 -0500

    Add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
+import AddInventory from './components/add-inventory.js'
+import InventoryList from './components/inventory-list.js'
+import Inventory from './components/inventory.js';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/inventory" classname="navbar-brand">
+          Inventory
+        </a>
+        {/* <div className="navbar-nav me-auto">
+          <li className="nav-item">
+            <Link to={"/inventory"} className="nav-link">
+              Inventory
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/inventory"} className="nav-link">
+              Inventory
+            </Link>
+          </li>
+        </div> */}
+      </nav>
     </div>
   );
 }
